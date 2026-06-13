0fa4e6c8d94570d75da144e758bfe1409a6d1f1e
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-13 16:31:37 +0500

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,45 +1,71 @@
 import appendQuery from 'append-query';
 
-export const isObject = (obj: any) => {
-  return typeof obj === 'object' && obj !== null
-}
+export const isObject = (value: unknown): value is Record<string, unknown> => {
+  return value !== null && typeof value === 'object';
+};
 
-export const getObjectType = (obj: any) => Object.prototype.toString.call(obj)
+export const getObjectType = (value: unknown): string => {
+  return Object.prototype.toString.call(value);
+};
 
-export const removeUrlPostfix = (url: string) => {
-  let res = url.split('?')?.[0] ?? ''
-  res = res.split('#')?.[0] ?? ''
-  return res
-}
+export const removeUrlPostfix = (url: string): string => {
+  const queryIndex = url.indexOf('?');
+  const hashIndex = url.indexOf('#');
 
-export const getUrlQuery = (url: string) => {
-  const queryStr = url.split('?')?.[1] ?? ''
-  const res = Object.fromEntries(
-    queryStr.split('&').map((part) => {
-      return part.split('=')
-    })
-  )
-  return res
-}
+  let endIndex = url.length;
+
+  if (queryIndex !== -1) {
+    endIndex = queryIndex;
+  }
+
+  if (hashIndex !== -1 && hashIndex < endIndex) {
+    endIndex = hashIndex;
+  }
+
+  return url.slice(0, endIndex);
+};
+
+export const getUrlQuery = (
+  url: string
+): Record<string, string> => {
+  const queryIndex = url.indexOf('?');
+
+  if (queryIndex === -1) {
+    return {};
+  }
+
+  const queryString = url
+    .slice(queryIndex + 1)
+    .split('#')[0];
+
+  return Object.fromEntries(
+    new URLSearchParams(queryString).entries()
+  );
+};
 
 // export const appendUrlQuery = (
 //   url: string,
-//   query: { [key: string]: any }
+//   query: Record<string, unknown>
 // ) => {
 //   return appendQuery(url, query)
 // }
 
 export const appendUrlQuery = (
   baseUrl: string,
-  params: Record<string, any>
+  params: Record<string, unknown>
 ): string => {
-  const Url = new URL(baseUrl);
-  const urlParams: URLSearchParams = new URLSearchParams(Url.search);
-  for (const key in params) {
-    if (params[key] !== undefined) {
-      urlParams.set(key, params[key]);
+  try {
+    const url = new URL(baseUrl);
+    const searchParams = url.searchParams;
+
+    for (const [key, value] of Object.entries(params)) {
+      if (value !== undefined && value !== null) {
+        searchParams.set(key, String(value));
+      }
     }
+
+    return url.toString();
+  } catch {
+    return appendQuery(baseUrl, params);
   }
-  Url.search = urlParams.toString();
-  return Url.toString();
 };
