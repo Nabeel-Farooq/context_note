3451a3690fcb9f7136bebfdc9a57740164855cdb
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-13 16:36:33 +0500

    Update tag.ts

--- src/types/tag.ts
+++ src/types/tag.ts
@@ -1,9 +1,10 @@
 export interface Tag {
-  id: string;
-  name: string;
-  color: string;
-  noteIds: string[];
-  updateTime: number;
-  isSelect?: boolean; // is the tag is selected or not
-  isCreating?: boolean; // is the tag is being created or not
+  readonly id: string;
+  readonly name: string;
+  readonly color: string;
+  readonly noteIds: readonly string[];
+  readonly updateTime: number;
+
+  readonly isSelect?: boolean;
+  readonly isCreating?: boolean;
 }
