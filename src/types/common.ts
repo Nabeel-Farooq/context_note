3ab12f76e4d32664e322966f5ce46cbba3872ab5
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-22 14:29:17 +0500

    Update common.ts

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,27 +1,19 @@
-export interface Coor {
-  readonly x: number;
-  readonly y: number;
+export interface Point {
+  readonly x: number
+  readonly y: number
 }
-
-export interface Rect {
-  readonly x: number;
-  readonly y: number;
-  readonly width: number;
-  readonly height: number;
-  readonly id?: string;
-}
-
-export interface Oper {
-  readonly title: string;
-  readonly onClick: () => void;
-  readonly isConfirm?: boolean;
-}
-
-export type Point = Coor;
 
 export interface Size {
-  readonly width: number;
-  readonly height: number;
+  readonly width: number
+  readonly height: number
 }
 
-export interface Rectangle extends Rect {}
+export interface Rect extends Point, Size {
+  readonly id?: string
+}
+
+export interface Operation {
+  readonly title: string
+  readonly onClick: () => void
+  readonly isConfirm?: boolean
+}
