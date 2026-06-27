a36d6a530d6ef9fd5dcb21815de8f8a4317f79c0
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-27 18:24:44 +0500

    Update common.ts

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,19 +1,19 @@
 export interface Point {
-  readonly x: number
-  readonly y: number
+  readonly x: number;
+  readonly y: number;
 }
 
 export interface Size {
-  readonly width: number
-  readonly height: number
+  readonly width: number;
+  readonly height: number;
 }
 
 export interface Rect extends Point, Size {
-  readonly id?: string
+  readonly id?: string;
 }
 
 export interface Operation {
-  readonly title: string
-  readonly onClick: () => void
-  readonly isConfirm?: boolean
+  readonly title: string;
+  readonly onClick: () => void;
+  readonly isConfirm?: boolean;
 }
