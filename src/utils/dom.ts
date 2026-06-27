208f9d6fcba5fcdec461198b801b25764ea0327e
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-27 18:22:40 +0500

    Update dom.ts

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,36 +1,19 @@
-import { getObjectType } from "../utils/utils";
-
 export const getNodeText = (node: Node | null | undefined): string => {
   if (!node) {
     return "";
   }
 
-  const objectType = getObjectType(node);
-
-  switch (objectType) {
-    case "[object HTMLTimeElement]":
-      return (node as HTMLTimeElement).dateTime?.trim() ?? "";
-
-    case "[object Text]":
-      return node.textContent?.trim() ?? "";
-
-    case "[object HTMLSpanElement]":
-      return (node as HTMLSpanElement).innerText?.trim() ?? "";
-
-    default: {
-      if (node instanceof Text) {
-        return node.textContent?.trim() ?? "";
-      }
-
-      if (node instanceof HTMLElement) {
-        if (node instanceof HTMLTimeElement) {
-          return node.dateTime?.trim() ?? "";
-        }
-
-        return node.innerText?.trim() ?? "";
-      }
-
-      return "";
-    }
+  if (node instanceof HTMLTimeElement) {
+    return node.dateTime.trim();
   }
+
+  if (node instanceof Text) {
+    return node.textContent?.trim() ?? "";
+  }
+
+  if (node instanceof HTMLElement) {
+    return node.innerText.trim();
+  }
+
+  return "";
 };
